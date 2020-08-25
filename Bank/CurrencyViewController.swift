a44ce8c538011c9fa46597ec612270992fdd6611
Andrew Frolov <adm.kolchak.221001@gmail.com>
2020-08-25 19:25:13 +0700

    app works with some bugs

--- Bank/CurrencyViewController.swift
+++ Bank/CurrencyViewController.swift
@@ -12,7 +12,7 @@ import SwiftyJSON
 
 class CurrencyViewController: UIViewController {
     
-    let currencyData = ["EUR","USD","JEP","GBP","IMP","CNY","BAM","AUD","NZD","JOD","CHF","BGN","KWD","MYR","XAU"]
+    let currencyData = ["EUR","USD","GBP", "JPY","CNY","BAM","AUD","NZD","JOD","CHF", "CAD","ALL","AMD","BIF","BND","BOB","BGN","KWD","MYR","XAU"]
     
     @IBOutlet weak var currencyTable: UITableView!
     @IBOutlet weak var date: UILabel!
@@ -25,63 +25,13 @@ class CurrencyViewController: UIViewController {
     var yesterdayDate = ""
     let constToBuy = -1.3
     let constToSell = 1.7
-
+    var eur = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        //dateToString()
-        //yesterdayInfo()
-        todayInfo()
-        //meanInfo()
-        date.text = currencyValueToday["date"].string
+        date.text = "\(currencyValueToday["date"])"
+        eur = currencyValueToday["rates"]["RUB"].doubleValue
     }
-    
-    func yesterdayInfo() {
-        AF.request("http://data.fixer.io/api/\(yesterdayDate)?access_key=eb10647117f5075162ee60ec9b2a3fb1").responseJSON {
-            responceJSON in
-            switch responceJSON.result {
-            case .success(let value):
-                print("Success got the yesterday value")
-                self.currencyValueYesterday = JSON(responceJSON.value)
-            case .failure(let error):
-                print(error)
-            }
-        }
-    }
-    
-    func todayInfo() {
-        AF.request("http://data.fixer.io/api/latest?access_key=eb10647117f5075162ee60ec9b2a3fb1").responseJSON {
-            responceJSON in
-            switch responceJSON.result {
-            case .success(let value):
-                print("Success got the today value")
-                self.currencyValueToday = JSON(responceJSON.value)
-            case .failure(let error):
-                print(error)
-            }
-        }
-    }
-    
-    func meanInfo() {
-        AF.request("http://data.fixer.io/api/symbols?access_key=eb10647117f5075162ee60ec9b2a3fb1").responseJSON {
-            responceJSON in
-            switch responceJSON.result {
-            case .success(let value):
-                print("Success got the mean value")
-                self.currencyMean = JSON(responceJSON.value)
-            case .failure(let error):
-                print(error)
-            }
-        }
-    }
-    
-    func dateToString() {
-        let yesterday = Calendar.current.date(byAdding: .day, value: -1, to: Date())
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
-        yesterdayDate = dateFormatter.string(from: yesterday!)
-    }
-
 }
 
 extension CurrencyViewController: UITableViewDelegate, UITableViewDataSource {
@@ -92,20 +42,24 @@ extension CurrencyViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "regularCell") as! RegularTableViewCell
         
-        let currencyValue = currencyData[indexPath.row]
-        
-        cell.currencyCode.text = currencyValue
-        cell.currencyName.text = currencyMean["symbols"]["\(currencyValue)"].string
-        cell.countryImage.image = UIImage(named: "USD")
-        //var todayPrice = currencyValueToday["rates"]["\(currencyValue)"].double
-        //var yesterdayPrice = currencyValueYesterday["rates"]["\(currencyValue)"].double
-        //todayPrice = roundDown(todayPrice!, toNearest: 0.01)
-        //yesterdayPrice = roundDown(yesterdayPrice!, toNearest: 0.01)
-        // проблема с unwrap
-        cell.buyIndicator.image = UIImage(named: "high")
-        cell.sellIndicator.image = UIImage(named: "low")
-        //cell.priceToBuy.text = String(todayPrice! + constToBuy)
-        //cell.priceToSell.text = String(todayPrice! + constToSell)
+        cell.currencyCode.text = currencyData[indexPath.row]
+        cell.currencyName.text = "\(currencyMean["symbols"]["\(currencyData[indexPath.row])"])"
+        cell.countryImage.image = UIImage(named: "\(currencyData[indexPath.row])")
+        var todayPrice = currencyValueToday["rates"]["\(currencyData[indexPath.row])"].doubleValue
+        var yesterdayPrice = currencyValueYesterday["rates"]["\(currencyData[indexPath.row])"].doubleValue
+        todayPrice = roundDown(todayPrice, toNearest: 0.01)
+        yesterdayPrice = roundDown(yesterdayPrice, toNearest: 0.01)
+        let valueFromRub = eur / todayPrice
+        if todayPrice > yesterdayPrice {
+            cell.buyIndicator.image = UIImage(named: "high")
+            cell.sellIndicator.image = UIImage(named: "high")
+        }
+        else {
+            cell.buyIndicator.image = UIImage(named: "low")
+            cell.sellIndicator.image = UIImage(named: "low")
+        }
+        cell.priceToBuy.text = String(valueFromRub + constToBuy)
+        cell.priceToSell.text = String(valueFromRub + constToSell)
         return cell
     }
     
--- Bank/ViewController.swift
+++ Bank/ViewController.swift
@@ -16,19 +16,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var usdFromButton: UILabel!
     @IBOutlet weak var eurFromButton: UILabel!
     
+    var yesterdayDate = ""
+    
+    var currencyValueToday : JSON!
+    var currencyValueYesterday : JSON!
+    var currencyMean : JSON!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         addBackgroundImage()
         informationFromCurrencyButton()
+        dateToString()
+        yesterdayInfo()
+        todayInfo()
+        meanInfo()
         // Do any additional setup after loading the view.
     }
     
-    
-    @IBAction func exchangerates(_ sender: UIButton) {
-        self.dismiss(animated: true, completion: nil)
-        let vc = storyboard?.instantiateViewController(identifier: "table") as! UIViewController
-        self.present(vc, animated: true)
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let dest = segue.destination as? CurrencyViewController {
+            dest.currencyMean = currencyMean
+            dest.currencyValueToday = currencyValueToday
+            dest.currencyValueYesterday = currencyValueYesterday
+        }
     }
     
     func addBackgroundImage() {
@@ -53,19 +64,78 @@ class ViewController: UIViewController {
     }
     
     func updateinformationFromCurrencyButton(json : JSON) {
-        var eur = json["rates"]["RUB"].double
-        var usd = json["rates"]["USD"].double
-        usd = roundDown(usd!, toNearest: 0.01)
-        eur = roundDown(eur!, toNearest: 0.01)
-        var usdFromRub = eur! / usd!
+        var eur = json["rates"]["RUB"].doubleValue
+        var usd = json["rates"]["USD"].doubleValue
+        usd = roundDown(usd, toNearest: 0.01)
+        eur = roundDown(eur, toNearest: 0.01)
+        var usdFromRub = eur / usd
         usdFromRub = roundDown(usdFromRub, toNearest: 0.01)
-        eurFromButton.text = "EUR \(eur!)"
+        eurFromButton.text = "EUR \(eur)"
         usdFromButton.text = "USD \(usdFromRub)"
         dateFromButton.text = "\(json["date"])"
     }
     
     func roundDown(_ value: Double, toNearest: Double) -> Double {
       return floor(value / toNearest) * toNearest
+    }
+    
+    func yesterdayInfo() {
+        AF.request("http://data.fixer.io/api/\(yesterdayDate)?access_key=eb10647117f5075162ee60ec9b2a3fb1").responseJSON {
+            responceJSON in
+            switch responceJSON.result {
+            case .success(let value):
+                print("Success got the yesterday value")
+                let currencyJSON : JSON = JSON(responceJSON.value)
+                self.updateYesterday(json: currencyJSON)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func todayInfo() {
+        AF.request("http://data.fixer.io/api/latest?access_key=eb10647117f5075162ee60ec9b2a3fb1").responseJSON {
+            responceJSON in
+            switch responceJSON.result {
+            case .success(let value):
+                print("Success got the today value")
+                let currencyJSON : JSON = JSON(responceJSON.value)
+                self.updateToday(json: currencyJSON)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func meanInfo() {
+        AF.request("http://data.fixer.io/api/symbols?access_key=eb10647117f5075162ee60ec9b2a3fb1").responseJSON {
+            responceJSON in
+            switch responceJSON.result {
+            case .success(let value):
+                print("Success got the mean value")
+                let currencyJSON : JSON = JSON(responceJSON.value)
+                self.updateMean(json: currencyJSON)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func dateToString() {
+        let yesterday = Calendar.current.date(byAdding: .day, value: -1, to: Date())
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        yesterdayDate = dateFormatter.string(from: yesterday!)
+    }
+    
+    func updateYesterday(json : JSON) {
+        currencyValueYesterday = json
+    }
+    func updateToday(json : JSON) {
+        currencyValueToday = json
+    }
+    func updateMean(json : JSON) {
+        currencyMean = json
     }
 
 }
